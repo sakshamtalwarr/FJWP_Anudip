f3c3183b84119a6907b492da89c7051473caea18
Saksham Talwar <sakshamtalwar375@gmail.com>
2025-02-11 14:21:53 +0530

    Adjustments were made

---
+++ Core JAVA/Assignment 1 10.02.2025/ATM.java
@@ -0,0 +1,13 @@
+// Assignment 1
+// ATM
+// - authentication -> ask the user to fill in the account number and pin, if authentication is successful then show user options for further operations, else give him two more chances to login and if he fails to authenticate then, block the transaction.
+
+// -> if the authentication is successful, then 
+// 	show options:-
+// 		-> 1. View Balance
+// 		-> 2. Change pin/Password
+// 		-> 3. Change Phone Number
+// 		-> 4. Deposit Money
+// 		-> 5. Withdraw Money
+// 		-> 6. Register/Create pin
+// 		-> 7. Logout
---
+++ Core JAVA/class as on 10.02.2025/ArmstrongDemo.java
@@ -0,0 +1,64 @@
+
+// input a range from user and print all the  armstrong numbers from that range
+public package pack1;
+
+import java.util.Scanner;
+
+public class ArmstrongDemo 
+{
+	public int countDigits(int num)  // num=123
+	{
+		int digits=0; // digits =0
+		while(num!=0) // 0 != 0 false
+		{
+			digits++; // digits = 0+1=1 +1= 2 +1=3
+			num/=10;  // num = 123/10; num = 12/10=1/10= 0
+		}
+		
+		
+		return digits; // 3
+	}
+	
+	public int calculatePower(int n, int p)
+	{
+		int result=1;
+		for(int i=1;i<=p;i++)
+		{
+			result*=n; // result=result*n
+		}
+		
+		return result;
+	}
+	public static void main(String[] args) 
+	{
+		
+		
+		ArmstrongDemo ad=new ArmstrongDemo();
+		//	check whether a number is armstrong or not
+		// step 1: the number of digits
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num=sc.nextInt();// input number : num=123
+		
+		int countDigits = ad.countDigits(num); // call function to count digits
+//		countDigits = countDigits(123);,  countDigits = 3
+		System.out.println("Number of Digits : "+countDigits);// 3
+		
+//		Step 2 : calculate the power of each digit and sum all the answers 
+		int sum=0;
+		
+		for(int x=num;x!=0;x/=10) // x=123
+		{
+			int digit = x%10; // 3
+			int result = ad.calculatePower(digit,countDigits);
+			System.out.println("Result of n : "+result);
+			sum+=result;
+			System.out.println("Sum : "+sum);
+//			x/=10;
+		}
+//		step 3: compare the value of sum with number to check if they are equal or not
+//		if both are same, then print number is armstrong
+		if(sum == num) System.out.println(num+" is an Armstrong number");
+		else System.out.println(num+" is not an Armstrong number");
+	}
+} 
